719659b30cbfd2e00d68e63369827ac1d5d3316e
PhoenixAlx <raskalakabra@gmail.com>
2016-10-18 10:01:36 +0100

    test database

--- modules/club/lib/js/driver.js
+++ modules/club/lib/js/driver.js
@@ -17,6 +17,7 @@
  */
  function loadInit(){
 
+
     // Insert two rows: (1,111) and (2,222)
     
      $.fn.datepicker.defaults.format = "dd/mm/yyyy";
@@ -87,30 +88,12 @@ function validate_date(dateInput){
 
 }
 function save(){
-    	var mybase = new alasql.Database('mybase');
-	mybase.exec('CREATE TABLE one (two INT)');
-	var insert2 = alasql.compile('INSERT INTO one values(?,?)','mybase');
-	insert2({a:1,b:2});
-	var res = mybase.exec("SELECT * FROM one");
-	console.log(res);
-	var data = [["Minsk",100000], ["Riga",200000]];
-	alasql("SELECT * INTO CSV('cities.csv',{headers:true}) FROM ?",[data]);
     
 
-    alert('Environment detected: ' + JSON.stringify({
-                                        alasqlPath: alasql.path,
-                                        isBrowser: alasql.utils.isBrowser,
-                                        isCordova: alasql.utils.isCordova,
-                                        isMeteor: alasql.utils.isMeteor,
-                                        isMeteorClient: alasql.utils.isMeteorClient,
-                                        isMeteorServer: alasql.utils.isMeteorServer,
-                                        isNode: alasql.utils.isNode,
-                                        isWebWorker: alasql.utils.isWebWorker,
-                                        resView:res
-                                    },null,4));
-     mybase.exec("SELECT * INTO CSV('prubq.csv') FROM one");
-     
-     
+	
+
+      
+
     //validate field, firs is not empty
      var name=$('#name').val();
      var dateCreation=$('#dateCreation').val();
@@ -125,96 +108,9 @@ function save(){
          errorMSG.innerHTML="";
      }//validate name that it isn't on database
      
-     /*       var namePerson=document.getElementById("namePerson");
-            var agePerson=document.getElementById("agePerson");
-    var db=loadDatabase();
-   
-                
-   var request = db.transaction(["student"], "readwrite")
-   .objectStore("student")
-   .add({ id: idPerson.value, name: namePerson.value, age: agePerson.value});
+     /*       
      
-   request.onsuccess = function(event) {
-      alert("New Record has been added to your database.");
-   };
-     
-   request.onerror = function(event) {
-      alert("Unable to add data\r\nRecord is already exist in database! ");
-   }*/
-}
-/*           
-         request.onupgradeneeded = function(event) {
-            var db = event.target.result;
-            var objectStore = db.createObjectStore("student", {keyPath: "id"});
-              
-            for (var i in studentData) {
-               objectStore.add(studentData[i]);
-            }
-         }
-           
-         function readData() {
-   var transaction = db.transaction(["student"]);
-   var objectStore = transaction.objectStore("student");
-   var request = objectStore.get("01");
-     
-   request.onerror = function(event) {
-      alert("Unable to retrieve data from database!");
-   };
-     
-   request.onsuccess = function(event) {
-      if(request.result) {
-         alert("Name: " + request.result.name + ", Age: " + request.result.age);
-      }
-        
-      else {
-         alert("Record not found in database!");
-      }
-   };
-}
-  
-function readAllData() {
-   var objectStore = db.transaction("student").objectStore("student");
-     
-   objectStore.openCursor().onsuccess = function(event) {
-      var cursor = event.target.result;
-        
-      if (cursor) {
-         alert("Name for id " + cursor.key + " is " + cursor.value.name + ", Age: " + cursor.value.age);
-         cursor.continue();
-      }      
-      else {
-         alert("No more records!");
-      }
-};
-}
-           
-         function addData() {
-            var idPerson=document.getElementById("idPerson");
-            var namePerson=document.getElementById("namePerson");
-            var agePerson=document.getElementById("agePerson");
-                
-   var request = db.transaction(["student"], "readwrite")
-   .objectStore("student")
-   .add({ id: idPerson.value, name: namePerson.value, age: agePerson.value});
-     
-   request.onsuccess = function(event) {
-      alert("New Record has been added to your database.");
-   };
-     
-   request.onerror = function(event) {
-      alert("Unable to add data\r\nRecord is already exist in database! ");
-   }
-}
-       
-   
-  function removeData() {
-   var request = db.transaction(["student"], "readwrite")
-   .objectStore("student")
-   .delete("01");
-     
-   request.onsuccess = function(event) {
-      alert("Record has been deleted from your database.");
-   };
-  
-}
+    	var mybase = new alasql.Database('mybase');
+
+	var res = mybase.exec("SELECT * FROM one");
     */
